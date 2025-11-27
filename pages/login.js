70d10d76bf4085e361fe2cf684f2489588d8a87c
Manjunatha Venkateshappa <manjunatha.venkateshappa@sandhata.com>
2025-11-27 14:21:34 +0530

    adding files

---
+++ pages/login.js
@@ -0,0 +1,29 @@
+exports.LoginPage = class loginPage {
+    constructor(page) {
+        this.page = page;
+        this.usernameInput = page.locator('text=Username or email');
+        this.passwordInput = page.locator('text=Password');
+        this.loginButton = page.getByRole('button', { name: 'Sign In' });
+    }
+    async gotoLoginPage() {
+        await this.page.goto('https://diam.test.digicom.vodafone.com/realms/dnext/protocol/openid-connect/auth?client_id=dnext-ui-client&redirect_uri=https%3A%2F%2Ftest.digicom.vodafone.com%2Fui%2FdigitalOrdering%2Forder-validation&state=5f684ad5-cdee-486e-8043-c9298463aafb&response_mode=fragment&response_type=code&scope=openid&nonce=0e4799bf-d477-4c99-bee5-7184419f0472');
+     }
+
+    async login(username, password) {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+    // async navigate() {
+    //     await this.page.goto('https://diam.test.digicom.vodafone.com/realms/dnext/protocol/openid-connect/auth?client_id=dnext-ui-client&redirect_uri=https%3A%2F%2Ftest.digicom.vodafone.com%2Fui%2FdigitalOrdering%2Forder-validation&state=5f684ad5-cdee-486e-8043-c9298463aafb&response_mode=fragment&response_type=code&scope=openid&nonce=0e4799bf-d477-4c99-bee5-7184419f0472');
+    // }
+    // async enterUsername(username) {
+    //     await this.usernameInput.fill("GCC1@vmailsink.dmm.vodafone.com");
+    // }
+    // async enterPassword(password) {
+    //     await this.passwordInput.fill("digicom123");
+    // }
+    // async clickLogin() {
+    //     await this.loginButton.click();
+    // }
+}
